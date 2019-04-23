4fd4e0089e37b5634ca131c23c152b4f38286344
marvin <mw1996oede@googlemail.com>
2019-04-23 15:12:45 +0200

    now allows 64 players by protocol, unfortunately that changes the game hash version and i have to overwrite it, fixes #4

--- src/game/version.h
+++ src/game/version.h
@@ -4,7 +4,7 @@
 #define GAME_VERSION_H
 #include <generated/nethash.cpp>
 #define GAME_VERSION "0.7.3"
-#define GAME_NETVERSION "0.7 " GAME_NETVERSION_HASH
+#define GAME_NETVERSION "0.7 802f1be60a05665f"
 #define CLIENT_VERSION 0x0703
 static const char GAME_RELEASE_VERSION[8] = "0.7.3";
 #define CATCH_VERSION "1.0.0"
